b955a074e296b600daa77c0c7b726be267b15420
Danny <danny.costelloe@gmail.com>
2021-06-23 17:13:44 -0400

    added scrollToTop

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
+import ScrollToTop from "./components/ScrollToTop";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
